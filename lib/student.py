2b865dce48f79f33cbf11acb6346a7f63ab16e19
Stella Mazzillo <stellagress5@gmail.com>
2023-07-20 01:12:23 -0400

    complete p3 inheritance lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,14 @@
 from user import User
 
 class Student(User):
+
+
+
+
+    def __init__(self, first_name, last_name, knowledge=[]):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+        
     
-    def learn(self):
-        pass
+    def learn(self, new_info):
+        self.knowledge.append(new_info)
